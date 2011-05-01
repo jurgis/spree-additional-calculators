291467b1004373f911f442d892d9c8d065a08478
Jurgis Jurksta <jurgis@emails.lv>
2011-05-01 20:33:37 +0300

    Migrated missing pieces

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # Add your extension routes here
+  namespace :admin do
+    resources :additional_calculator_rates, :only => [ :index, :edit, :update ]
+  end
 end
--- lib/spree_additional_calculators_hooks.rb
+++ lib/spree_additional_calculators_hooks.rb
@@ -1,3 +1,12 @@
 class SpreeAdditionalCalculatorsHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
+
+  # Add a link on admin configuration page
+  insert_after :admin_configurations_menu do
+    %(<%= configurations_menu_item(t("additional_calculator_rates"), admin_additional_calculator_rates_path, t("additional_calculator_rates_description")) %>)
+  end
+
+  # Add js file to the head section
+  insert_after :admin_inside_head, 'shared/additional_calculators_admin_head'
+
 end
